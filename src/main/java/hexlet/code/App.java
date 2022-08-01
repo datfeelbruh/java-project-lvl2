073c1b328476072aa973f71239fc919be1459a3d
khoroshko.d <datfeelman@gmail.com>
2022-08-01 17:46:48 +0300

    update picocli interactions

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -12,6 +12,26 @@ import picocli.CommandLine.Parameters;
 
 
 public class App implements Runnable {
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    String format = "stylish";
+
+    @Parameters(
+            paramLabel = "filepath1",
+            description = "path to first file"
+    )
+    String filepath1;
+
+    @Parameters(
+            paramLabel = "filepath2",
+            description = "path to second file"
+    )
+    String filepath2;
+
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
